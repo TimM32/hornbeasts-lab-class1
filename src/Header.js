578c6f3f2e65c27c39ae6051210544486f2c4417
TimM32 <timmaupin@live.com>
2023-04-17 13:11:47 -0500

    finish adding header and h1 in header.js

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,20 @@ import React from "react";
 
 
 class Header extends React.Component {
+    render () {
+        return (
+            <>
+            <header>
+                <h1>Hornbeasts 301 </h1>
+            </header>
+            </>
 
+
+
+        );
+
+
+    }
 
 
 
