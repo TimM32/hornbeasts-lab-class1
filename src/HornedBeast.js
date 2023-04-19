5de9e4730140d55c0462f18abf014ec1c5c5f980
TimM32 <timmaupin@live.com>
2023-04-18 21:41:18 -0500

    add in hornedbeast.css

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -22,11 +22,11 @@ class HornedBeast extends React.Component {
     return (
       <>
         <article>
-           <h2>Animal: {this.props.title}</h2>
-           <p>Favorites {this.state.favorites}</p>
-           <Button onClick={this.handleFavorites}> 💖 </Button>
-           <img src={this.props.imageUrl} alt={this.props.title} />
-           <p>Description: {this.props.description}</p>
+          <h2>Animal: {this.props.title}</h2>
+          <p>Favorites {this.state.favorites}</p>
+          <Button onClick={this.handleFavorites}> 💖 </Button>
+          <img src={this.props.imageUrl} alt={this.props.title} />
+          <p>Description: {this.props.description}</p>
         </article>
       </>
     );
