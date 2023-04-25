0d068552eabea4934b4122b95d913f75dfd5c24a
TimM32 <timmaupin@live.com>
2023-04-25 13:12:24 -0500

    working on getting modal to work

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,7 @@
 import React from "react";
 import HornedBeast from "./HornedBeast";
 import data from './data.json';
+import { Container, Row } from 'react-bootstrap';
 import './main.css';
 
 
@@ -8,26 +9,30 @@ import './main.css';
 class Main extends React.Component {
     render () {
         let beast = [];
-        data.forEach((newBeast, index) => {
-            beast.push(<HornedBeast title={newBeast.title} imageUrl={newBeast.imageUrl} description={newBeast.description} key={index} />)
+        this.props.data.map((newBeast, index) => {
+            return beast.push(
+                <HornedBeast 
+                title={newBeast.title} 
+                image_url={newBeast.image_url} 
+                description={newBeast.description}
+                key={index}
+                handleOnShow={this.props.handleOnShow}
+                data={this.props.data}
+                 />
+            )
         });
 
 
-        return (
-            <main>
-                
-                {/* // <HornedBeast 
-                // title="Rhino Family"
-                //  imageUrl="https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80" description="From Africa"/>
-                // <HornedBeast 
-                // title="Unicorn Head"
-                // imageUrl="https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg"
-                // description="Wild animal that loves cars"
-                />  */}
-                {HornedBeast}
-            </main>
-        );
+            return (
+                <main>
+                    <Container>
+                        <Row lg={4} md={3} sm={2} xs={1}>
+                            {beast}
+                        </Row>
+                    </Container>
+                </main>
+            );
+        }
     }
-}
 
 export default Main;
